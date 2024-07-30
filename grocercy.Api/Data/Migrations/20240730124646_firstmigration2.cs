96dfab8b1993729bc066399646b4d786ee97499b
wael <wael.fci_1181@fci.kfs.edu.eg>
2024-07-30 16:47:08 +0300

    fdsfsdf

---
+++ grocercy.Api/Data/Migrations/20240730124646_firstmigration2.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grocercy.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)1,
+                column: "Image",
+                value: "fruits.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)2,
+                column: "Image",
+                value: "seasonal_fruits.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)3,
+                column: "Image",
+                value: "exotic_fruits.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)4,
+                column: "Image",
+                value: "vegetables.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)5,
+                column: "Image",
+                value: "green_vegetables.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)6,
+                column: "Image",
+                value: "leafy_vegetables.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)7,
+                column: "Image",
+                value: "salads.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)8,
+                column: "Image",
+                value: "dairy.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)9,
+                column: "Image",
+                value: "milk_curd_yogurt.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)10,
+                column: "Image",
+                value: "butter_cheese.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Color",
+                value: "#7fbdc7");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Color",
+                value: "#e28083");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Color",
+                value: "#7fbdc7");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)1,
+                column: "Image",
+                value: "fruits.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)2,
+                column: "Image",
+                value: "seasonal_fruits.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)3,
+                column: "Image",
+                value: "exotic_fruits.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)4,
+                column: "Image",
+                value: "vegetables.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)5,
+                column: "Image",
+                value: "green_vegetables.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)6,
+                column: "Image",
+                value: "leafy_vegetables.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)7,
+                column: "Image",
+                value: "salads.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)8,
+                column: "Image",
+                value: "dairy.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)9,
+                column: "Image",
+                value: "milk_curd_yogurt.png");
+
+            migrationBuilder.UpdateData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: (short)10,
+                column: "Image",
+                value: "butter_cheese.png");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Color",
+                value: "#e1f1e7");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Color",
+                value: "#ea978d");
+
+            migrationBuilder.UpdateData(
+                table: "Offer",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Color",
+                value: "#dad1f9");
+        }
+    }
+}
--- groceryApp/Models/CategoryModel.cs
+++ groceryApp/Models/CategoryModel.cs
@@ -22,8 +22,21 @@ namespace groceryApp.Models
         public string Credit { get; set; }
         public short Id { get; set; }
         public string Name { get; set; }
-        public string Image { get; set; }
+        private string image;
+        
         public short ParentId { get; set; }
         public bool IsMainCategory => ParentId == 0;
+
+        public string Image
+        {
+            get => image; 
+            set
+            {
+               if(image != value)
+                {
+                    image = $"https://github.com/Abhayprince/FruitVegBasketMAUI/blob/master/FruitVegBasket.Api/wwwroot/images/categories/{value}";
+                }   
+            } 
+        }
     }
 }
